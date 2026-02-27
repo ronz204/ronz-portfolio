53d9ee2a4e0dcf46bf34f6c3eb109d2ca49525e7
roney ruiz <roneyruizrojas@gmail.com>
2026-02-26 22:20:38 -0600

    feat: discover nuxt routing and try tailwind

---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
+    <p class="text-lg text-gray-600">Explore my projects and experience.</p>
+  </div>
+</template>
