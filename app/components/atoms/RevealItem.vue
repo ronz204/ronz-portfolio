493aef06a2f288ea8686c56ea57ec1a27ef3b538
roney ruiz <roneyruizrojas@gmail.com>
2026-02-28 15:17:54 -0600

    feat: create reveal item and section atoms

---
+++ app/components/atoms/RevealItem.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  delay?: number
+}>(), {
+  delay: 0,
+});
+</script>
+
+<template>
+  <div data-reveal
+    class="opacity-0 translate-y-6 transition-all duration-700 ease-out [&.is-visible]:opacity-100 [&.is-visible]:translate-y-0"
+    :style="{ transitionDelay: `${props.delay}ms` }">
+    <slot />
+  </div>
+</template>
---
+++ app/components/atoms/RevealSection.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useScrolling } from "~/composables/useScrolling";
+const containerRef = useScrolling();
+</script>
+
+<template>
+  <div ref="containerRef">
+    <slot />
+  </div>
+</template>
